bc5f396aec8bc53b4414bcb15f7a50d6a8303662
Aliiiiii87 <134264112+Aliiiiii87@users.noreply.github.com>
2023-10-09 12:45:37 +0200

    Homefragment

--- app/src/main/java/com/example/mindmaster/ui/CourseFragment.kt
+++ app/src/main/java/com/example/mindmaster/ui/CourseFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mindmaster.ui
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,12 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import com.example.mindmaster.R
-import com.example.mindmaster.adapter.HomeAdapter
-import com.example.mindmaster.databinding.FragmentCategoryBinding
 import com.example.mindmaster.databinding.FragmentCourseBinding
 
 
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+
 
 class CourseFragment : Fragment() {
 
@@ -40,6 +41,20 @@ class CourseFragment : Fragment() {
             // Hier können Sie die Auswertungsnachricht in Ihrem UI anzeigen, z.B. in einem TextView
             binding.courseTV.text = evaluationMessage
         }
+
+        val videoPath = "android.resource://${requireContext().packageName}/${R.raw.money}"
+
+        binding.videoView.setVideoURI(Uri.parse(videoPath))
+        binding.videoView.start()
+
+
+        // Das ist der Wiederholungslistener für das Video
+        binding.videoView.setOnCompletionListener { mediyaPlayer->
+
+            mediyaPlayer.start()
+        }
+
+
 
 
 
